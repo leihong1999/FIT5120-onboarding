432a6773e63997c5ed3f66e98e331c861b3e62f2
Lei Hong <lhon0005@student.monash.edu>
2023-08-02 16:41:51 +1000

    welcome page

---
+++ my-app/src/Page1.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Page-1.css';
+import './nicepage.css';
+
+function Page1() {
+    return (
+        <section class="u-align-center u-clearfix u-section-1" id="sec-9e1e">
+            <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+                <div class="u-clearfix u-expanded-width u-gutter-0 u-layout-wrap u-layout-wrap-1">
+                    <div class="u-layout">
+                        <div class="u-layout-row">
+                            <div class="u-align-left u-container-style u-image u-layout-cell u-left-cell u-size-30 u-size-xs-60 u-image-1" src="" data-image-width="1280" data-image-height="854">
+                                <div class="u-container-layout u-valign-middle u-container-layout-1" src=""></div>
+                            </div>
+                            <div class="u-align-left u-container-style u-layout-cell u-palette-4-base u-right-cell u-size-30 u-size-xs-60 u-layout-cell-2">
+                                <div class="u-container-layout u-valign-middle u-container-layout-2">
+                                    <h2 class="u-text u-text-default u-text-1">App Name</h2>
+                                    <p class="u-text u-text-2">Sample text. Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices.</p>
+                                    <a href="" class="u-border-2 u-border-white u-btn u-btn-rectangle u-button-style u-none u-btn-1">Start</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Page1;
