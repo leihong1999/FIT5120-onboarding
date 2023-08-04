a1af4fdff24ce3a86d83ce7d69fabfa4e0a48cfa
Hong Lei <honglei@dyn-118-139-47-24.its.monash.edu.au>
2023-08-04 14:33:45 +1000

    linkjump

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,16 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Page1 from './Page1';
+import Page2 from './Page2';
+
 function App() {
   return (
-    <Page1 />
+    <Router>
+      <Routes>
+        <Route path="FIT5120-onboarding/" element={<Page1 />} />
+        <Route path="/page2" element={<Page2 />} />
+      </Routes>
+    </Router>
   );
 }
 
